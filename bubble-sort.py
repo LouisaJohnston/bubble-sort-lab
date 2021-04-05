5cc11565de356c2d6918aa9b92f3034306471a8f
LouisaJohnston <louisa.latham.johnston@gmail.com>
2021-04-05 11:38:02 -0700

    finish lab

--- bubble-sort.py
+++ bubble-sort.py
@@ -1,5 +1,9 @@
 def bubble_sort(arr):
-  pass # Delete this line and replace it with your implementation.
+  n = len(arr)
+  for i in range(n-1):
+    for j in range(0, n-i-1):
+      if arr[j] > arr[j+1]:
+        arr[j], arr[j+1] = arr[j+1], arr[j]
 
 nums = [5, 3, 8, 6]
 bubble_sort(nums)
